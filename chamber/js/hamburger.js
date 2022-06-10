5f9eaee49d89c8d0516c2fdedc964cd7dfeb1f64
hexaF6-NinjaMC <bec21016@byui.edu>
2022-06-10 01:54:42 -0500

    updated through lesson 4

--- chamber/js/hamburger.js
+++ chamber/js/hamburger.js
@@ -2,8 +2,8 @@ function toggleMenu() {
     document.getElementsByClassName('navigation')[0].classList.toggle('responsive')
     
     if (document.getElementsByClassName('responsive')[0]) {
-        document.getElementsByTagName('a')[0].innerHTML = '&#10006; CLOSE';
+        document.getElementsByClassName('toggler')[0].innerHTML = '&#10006; CLOSE';
     } else {
-        document.getElementsByTagName('a')[0].innerHTML = '&#9776; MENU';
+        document.getElementsByClassName('toggler')[0].innerHTML = '&#9776; MENU';
     }
 }

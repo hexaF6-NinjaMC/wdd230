34188aea9cbef45d9cdea326e3035d93443925ab
hexaF6-NinjaMC <bec21016@byui.edu>
2022-06-23 15:02:58 -0500

    lesson 7 lazy images activity

---
+++ lesson7/js/lazy-load.js
@@ -0,0 +1,25 @@
+const images = document.querySelectorAll('[data-src]');
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+
+    images.forEach((image) => {
+        observer.observe(image);
+    });
+} else {
+    images.forEach((image) => {
+        loadImages(image);
+    });
+}
